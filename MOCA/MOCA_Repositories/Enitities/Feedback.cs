1d9831473655294941c02564cc0ed5a5a439b079
Hung <khanhhungzm@gmail.com>
2025-05-30 22:30:27 +0700

    update database

--- MOCA/MOCA_Repositories/Enitities/Feedback.cs
+++ MOCA/MOCA_Repositories/Enitities/Feedback.cs
@@ -7,11 +7,15 @@ public partial class Feedback
 {
     public int FeedbackId { get; set; }
 
-    public string? UserName { get; set; }
+    public int? UserId { get; set; }
+
+    public int? CourseId { get; set; }
 
     public int? Rating { get; set; }
 
     public string? Comment { get; set; }
 
     public DateTime? CreateDate { get; set; }
+
+    public virtual Course? Course { get; set; }
 }
